5dd5deb551f15b5faa4a91ec05f0897db1edef14
teru12012000 <sion2769@gmail.com>
2024-03-25 23:04:29 +0900

    fix:grobal css

--- project/src/app/global.css.ts
+++ project/src/app/global.css.ts
@@ -3,5 +3,6 @@ import { globalStyle } from "@vanilla-extract/css"
 globalStyle("*", {
     margin: 0,
     boxSizing: "border-box",
-    padding: 0
+    padding: 0,
+    backgroundColor: "black"
 })
