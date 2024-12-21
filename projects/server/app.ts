da79b547407cc2f73090d9a9e6065195737d5d11
toshi119 <kakakatoshi119@gmail.com>
2024-12-21 12:29:08 +0900

    install dotenv

--- projects/server/app.ts
+++ projects/server/app.ts
@@ -1,11 +1,15 @@
 import express = require("express")
+import dotenv from "dotenv"
+
+dotenv.config()
+
 const app = express()
-const port = 8080
+const port = process.env.PORT
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
