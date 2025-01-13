ad68763e4d9a5027924f36d49873aa22599641bd
toshi119 <kakakatoshi119@gmail.com>
2025-01-13 11:12:42 +0900

    pnpm fmt

--- projects/server/.eslintrc.cjs
+++ projects/server/.eslintrc.cjs
@@ -1,58 +1,54 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
     },
-  },
-  plugins: [
-    "unused-imports",
-  ],
-  extends: [
-    "eslint:recommended",
-    "standard",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:regexp/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:deprecation/recommended",
-    "prettier",
-  ],
-  env: { browser: true },
-  ignorePatterns: ['dist', '.eslintrc.cjs', "vite.config.ts"],
-  rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-        destructuredArrayIgnorePattern: "^_",
-      },
+    plugins: ["unused-imports"],
+    extends: [
+        "eslint:recommended",
+        "standard",
+        "plugin:import/recommended",
+        "plugin:import/typescript",
+        "plugin:regexp/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:deprecation/recommended",
+        "prettier"
     ],
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/no-unnecessary-condition": "error",
-    "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+    env: { browser: true },
+    ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts"],
+    rules: {
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+                caughtErrorsIgnorePattern: "^_",
+                destructuredArrayIgnorePattern: "^_"
+            }
+        ],
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/no-floating-promises": "error",
+        "@typescript-eslint/no-unnecessary-condition": "error",
+        "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
 
-    "no-console": "warn",
-    "unused-imports/no-unused-imports": "error",
-    "import/default": "off",
-    "import/no-named-as-default-member": "off",
-    "import/order": "error",
+        "no-console": "warn",
+        "unused-imports/no-unused-imports": "error",
+        "import/default": "off",
+        "import/no-named-as-default-member": "off",
+        "import/order": "error",
 
-    "no-unreachable": "error",
+        "no-unreachable": "error",
 
-    "no-void": "off",
-    "camelcase": "error",
-    "deprecation/deprecation": "warn",
-    "@typescript-eslint/restrict-plus-operands": [
-      "error",
-    ],
-  },
-}
+        "no-void": "off",
+        camelcase: "error",
+        "deprecation/deprecation": "warn",
+        "@typescript-eslint/restrict-plus-operands": ["error"]
+    }
+};
--- projects/server/vite.config.ts
+++ projects/server/vite.config.ts
@@ -1,13 +1,11 @@
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-  ],
-  test: {
-    globals: true,
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    environment: 'node',
-  },
+    plugins: [tsconfigPaths()],
+    test: {
+        globals: true,
+        include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        environment: "node"
+    }
 });
