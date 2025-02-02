cf0cd1c511efdf4ea79294ba8e2489a59b1faa70
toshi119 <kakakatoshi119@gmail.com>
2025-02-02 16:27:06 +0900

    exclude some files but it doesn's work well...

--- projects/server/vite.config.ts
+++ projects/server/vite.config.ts
@@ -1,11 +1,17 @@
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    plugins: [tsconfigPaths()],
     test: {
         globals: true,
         include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        exclude: [
+            "package.json",
+            "pnpm-lock.yaml",
+            ".eslintrc.cjs",
+            ".prettierrc.json",
+            "tsconfig.json",
+            "vite.config.ts"
+        ],
         environment: "node"
     }
 });
