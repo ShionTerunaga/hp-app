2b034c497025dd6acd7fcd9f827f44fb7c393b4b
toshi119 <kakakatoshi119@gmail.com>
2025-02-09 17:34:49 +0900

    add: app.set views

--- projects/server/src/server.ts
+++ projects/server/src/server.ts
@@ -7,11 +7,14 @@ import { hogeController } from "./controllers/hogeController";
 const app = express();
 const port = appConfig.port;
 
+app.set("view engine", "ejs");
+app.set('views', './src/views')
+
 app.use(cors());
 app.use(express.json());
 
 app.use(appConfig.router.sample.base, sampleRouter);
-app.use(appConfig.router.hoge.base, hogeController);
+app.get(appConfig.router.hoge.base, hogeController);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
