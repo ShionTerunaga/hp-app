f1ca25b003725c146014758fef449b1cf0b3a6e9
teru12012000 <sion2769@gmail.com>
2025-03-26 08:57:58 +0900

    feat: create fetch

--- projects/client/src/app/(home)/layout/home-layout.tsx
+++ projects/client/src/app/(home)/layout/home-layout.tsx
@@ -1,7 +1,16 @@
 import { ja } from "@/shared/lang/ja";
 import styles from "./styles.css";
+import { getCharacter } from "@/feature/member-list/services/get-character";
+import { RESULT_NG } from "@/lib/core/result";
 
-export function HomeLayout() {
+export async function HomeLayout() {
+    const characters = await getCharacter();
+
+    if (characters.kind === RESULT_NG) {
+        return;
+    }
+
+    console.log(characters.value);
     return (
         <main>
             <h1 className={styles.heading}>{ja.home.title}</h1>
