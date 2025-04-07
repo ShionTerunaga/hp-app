4185fdcd315d13a89b1c12bf4ed736fbe5656f5a
TERUSI <69517378+ShionTerunaga@users.noreply.github.com>
2025-04-07 23:56:32 +0900

    feat: judge types (#63)

--- projects/client/src/feature/member-list/services/get-character.ts
+++ projects/client/src/feature/member-list/services/get-character.ts
@@ -1,7 +1,7 @@
 import { CustomError } from "@/lib/services/custom-error";
 import { ErrorHandler } from "@/lib/services/error-handler";
 import { appConfig } from "@/shared/config";
-import { APIRes } from "./response.type";
+import { APIRes, APIScheme } from "./response.type";
 import { ParseRes } from "./parse";
 import { createResult, Result } from "@/lib/core/result";
 import { UsePotter } from "./mock-api.type";
@@ -15,9 +15,16 @@ export async function getCharacter(): Promise<
         return ErrorHandler({ status: res.status });
     }
 
-    const body = (await res.json()) as APIRes[];
+    const body = await res.json();
 
-    const parseBody = ParseRes(body);
+    const typeResult = APIScheme.safeParse(body);
+
+    if (!typeResult.success) {
+        //後で詳細のエラーを作る
+        throw new Error("API型が違います");
+    }
+
+    const parseBody = ParseRes(typeResult.data);
 
     return createResult.ok(parseBody);
 }
